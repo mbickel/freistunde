5a1d527bb0c2e05c9cc1189c6cb16439f353ac24
Bettina Bickel <betti.huebner@gmail.com>
2020-03-22 13:34:46 +0100

    added logo

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     let elems = document.querySelectorAll('.collapsible');
     let instances = M.Collapsible.init(elems);
 });
+
+
+function scrollDown(){
+    alert("Hallo");
+}
