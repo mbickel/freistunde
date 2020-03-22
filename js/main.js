46f05f98d7012efb9f8d5318bdb273274fe33072
Bettina Bickel <betti.huebner@gmail.com>
2020-03-22 14:01:15 +0100

    First texts

--- js/main.js
+++ js/main.js
@@ -2,8 +2,3 @@ document.addEventListener('DOMContentLoaded', function() {
     let elems = document.querySelectorAll('.collapsible');
     let instances = M.Collapsible.init(elems);
 });
-
-
-function scrollDown(){
-    alert("Hallo");
-}
